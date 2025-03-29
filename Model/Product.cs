5fc048731dbc0c851539efd9bd1ba4fbc34016b3
QuangHa-PQH <quanghaneo7@gmail.com>
2025-03-29 08:30:08 +0700

    thbuoi2

---
+++ Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Phamquangha_2122110195_1.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
